c10adbf28e00b9c16602ce1df9d0f696de059a6b
Vishu Kodikanti <91806555+vish-the-fish737@users.noreply.github.com>
2023-06-03 00:28:21 -0500

    buttons and tabs

---
+++ script.js
@@ -0,0 +1,13 @@
+function openTab(evt, tabName) {
+    var i, tabContent, tabButtons;
+    tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+      tabContent[i].style.display = "none";
+    }
+    tabButtons = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabButtons.length; i++) {
+      tabButtons[i].className = tabButtons[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
